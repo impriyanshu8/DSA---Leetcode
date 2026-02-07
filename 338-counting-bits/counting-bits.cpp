8b6377bb2e256637ff6dc8f65c35b75279d289c7
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2026-02-07 16:13:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.02%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    int countsetBits(int n){
-        int count = 0;
-        while(n > 0){
-            count += (n & 1);
-            n = n >> 1;
-        }
-
-        if(n == 1) count += 1;
-        return count;
-    }
+  
     vector<int> countBits(int n) {
-        vector<int> res(n+1);
+        vector<int> res(n+1,0);
         for(int i=0; i<=n; i++){
-            res[i] = countsetBits(i);
+            res[i] = res[i >> 1] + (i & 1);
         }
         return res;
     }
