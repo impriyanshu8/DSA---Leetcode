292b03fc996d8ac7044be6306e47865db5564709
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2026-02-08 03:03:06 +0530

    Time: 1 ms (46.40%) | Memory: 9.9 MB (62.11%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    void generateSubSets(int index,vector<int> &nums,vector<int> &curr,vector<vector<int>> &ans){
-        if(index == nums.size()){
-            ans.push_back(curr);
-            return;
-        }
-        generateSubSets(index+1,nums,curr,ans);
-        curr.push_back(nums[index]);
-        generateSubSets(index+1,nums,curr,ans);
-        curr.pop_back();
-       
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>> ans;
-       vector<int> curr;
-       generateSubSets(0,nums,curr,ans);
-       return ans;
+        int n = nums.size();
+        int subsets = 1<<n;
+        vector<vector<int>>ans;
+        for(int num=0; num<subsets; num++){
+            vector<int> list;
+            for(int i=0; i<n; i++){
+                if(num & (1<<i)){
+                    list.push_back(nums[i]);
+                }
+            }
+            ans.push_back(list);
+        }
+        return ans;
     }
 };
