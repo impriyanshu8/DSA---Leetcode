65cdb07ee8cb121e801cc9962d5b5ca0e33d13b1
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2026-03-14 22:39:52 +0530

    Time: 1 ms (57.69%) | Memory: 8.5 MB (65.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int dup = x;
+        long long rev = 0;
+        while(x > 0){
+            int ld = x%10;
+            rev = rev * 10 + ld;
+            x = x/10;
+        }
+        if(dup == rev) return true;
+        else return false ;
+    }
+};
